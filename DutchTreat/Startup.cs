9ae448b481ed10f468917cee5b924cf101f1dc3a
Marc Ochsner <marcaochsner@gmail.com>
2020-09-28 13:53:08 -0500

    various updates for js/css guide

--- DutchTreat/Startup.cs
+++ DutchTreat/Startup.cs
@@ -39,6 +39,7 @@ namespace DutchTreat
             //});
             app.UseDefaultFiles();
             app.UseStaticFiles();
+            app.UseNodeModules();
         }
     }
 }
---
+++ DutchTreat/wwwroot/js/index.js
@@ -0,0 +1,18 @@
+﻿var x = 0;
+var s = "";
+
+alert("hello pluralsight");
+
+
+var theForm = document.getElementById("theForm");
+theForm.hidden = true;
+
+var button = document.getElementById("buyButton");
+button.addEventListener("click", function () {
+    alert("Buying item");
+});
+
+var productInfo = document.getElementsByClassName("product-props");
+var listItems = productInfo.item[0].children;
+
+
