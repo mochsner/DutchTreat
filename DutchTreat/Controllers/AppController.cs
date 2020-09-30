18d1bce9e610402ba68386361904b71ab9a347cb
Marc Ochsner <marcaochsner@gmail.com>
2020-09-30 10:08:52 -0500

    Complete MVC 6

--- DutchTreat/Controllers/AppController.cs
+++ DutchTreat/Controllers/AppController.cs
@@ -11,6 +11,8 @@ namespace DutchTreat.Controllers
     {
         public IActionResult Index()
         {
+            throw new InvalidOperationException();
+
             return View();
         }
     }
--- DutchTreat/Startup.cs
+++ DutchTreat/Startup.cs
@@ -22,6 +22,15 @@ namespace DutchTreat
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            if (env.IsEnvironment("Development"))
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // Add Error Page
+            }
+            app.UseDeveloperExceptionPage();
             app.UseStaticFiles();
             app.UseNodeModules();
 
